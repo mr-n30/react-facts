7604a1cbf64384f8d0bce27817c1676c54e78703
archie <archie@localhost>
2024-12-27 08:00:43 -0600

    Added 'base' key to vite.config.js to fix path bugs not loading properly

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  home: "https://github.com/mr-n30/react-facts"
+  base: "/react-facts"
 })

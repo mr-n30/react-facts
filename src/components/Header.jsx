1c86a6962bb7954cb4cc2a9e1e18b2735f6346a9
archie <archie@localhost>
2024-12-27 06:20:52 -0600

    Fixed image paths

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header>
       <nav>
-        <img src="/react-facts/public/images/react.svg" alt="ReactFacts Logo" className="nav-logo" />
+        <img src="./images/react.svg" alt="ReactFacts Logo" className="nav-logo" />
         <span>ReactFacts</span>
       </nav>
     </header>

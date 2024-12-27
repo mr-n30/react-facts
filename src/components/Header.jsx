3f245522de483bf5de509054862924de0cb22c81
archie <archie@localhost>
2024-12-27 07:49:39 -0600

    Update image paths to work with gh-pages / production environment and dev environment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header>
       <nav>
-        <img src="/react-facts/src/assets/images/react.svg" alt="ReactFacts Logo" className="nav-logo" />
+        <img src="/images/react.svg" alt="ReactFacts Logo" className="nav-logo" />
         <span>ReactFacts</span>
       </nav>
     </header>
